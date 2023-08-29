1561364d3d125d4693ac9e0c87f0aacca53e26be
carlynorama <carlynorama@gmail.com>
2023-08-29 11:33:15 -0700

    delete commented out imports

--- Sources/SerialSession/HardwareLayer/SerialPortConfiguration.swift
+++ Sources/SerialSession/HardwareLayer/SerialPortConfiguration.swift
@@ -5,10 +5,6 @@
 //  Created by Carlyn Maw on 8/25/23.
 //
 
-//import Foundation
-//import SwiftSerial
-
-
 public struct SerialPortConfiguration {
     let baudRate: CInt?
     let readEscape: (timeOut:UInt8, byteMin:UInt8)?
